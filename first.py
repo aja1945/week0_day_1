473c90948bb54f6797359df4431143d603fb8ff2
Andrew Ayala <aja1945@gmail.com>
2023-08-15 17:18:33 -0400

    message

--- first.py
+++ first.py
@@ -1,3 +1,6 @@
 # This is our first actual file in here!
 def hey(s_name):
-    return f"hello there{s_name}"
+    return f"hello there{s_name}" 
+
+def next(s_name):
+    return f"hi again {s_name}"
